95ce152045d00dc3a626e5523a8f1e1841656b92
Lmaokhushi <99333620+Lmaokhushi@users.noreply.github.com>
2022-02-10 03:23:28 +0530

    Add files via upload

---
+++ kk.py
@@ -0,0 +1,43 @@
+import random
+a = "Y"
+
+while a == "Y":
+    x = random.randint(1,6)
+    if x == 1:
+        print("---------")
+        print("|       |")
+        print("|   0   |")
+        print("|       |")
+        print("---------")
+    if x == 2:
+        print("---------")
+        print("|   0   |")
+        print("|       |")
+        print("|   0   |")
+        print("---------")
+    if x == 3:
+        print("---------")
+        print("|   0   |")
+        print("|       |")
+        print("| 0   0 |")
+        print("---------")
+    if x == 4:
+        print("---------")
+        print("| 0   0 |")
+        print("|       |")
+        print("| 0   0 |")
+        print("---------")
+    if x == 5:
+        print("---------")
+        print("| 0   0 |")
+        print("|   0   |")
+        print("| 0   0 |")
+        print("---------")
+    if x == 6:
+        print("---------")
+        print("| 0   0 |")
+        print("| 0   0 |")
+        print("| 0   0 |")
+        print("---------")
+    a = input("enter Y to roll the dice:")
+
